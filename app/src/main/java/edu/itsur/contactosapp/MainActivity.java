16dc04dc4469250d149a783d5404afbd4171301b
ozwaldo <ozwaldo@gmail.com>
2019-01-28 12:50:53 -0600

    2

--- app/src/main/java/edu/itsur/contactosapp/MainActivity.java
+++ app/src/main/java/edu/itsur/contactosapp/MainActivity.java
@@ -1,14 +1,22 @@
 package edu.itsur.contactosapp;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.provider.ContactsContract;
+import android.provider.ContactsContract.Contacts;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int PICK_CONTACT_REQUEST = 1;
+    private Uri contactoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,52 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void initSeleccionarContacto(View v) {
-        Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        Intent i = new Intent(Intent.ACTION_PICK, Contacts.CONTENT_URI);
         startActivityForResult(i, PICK_CONTACT_REQUEST);
     }
+
+    private void recibirConctacto(Uri uri) {
+        TextView nombre = (TextView) findViewById(R.id.nombreContacto);
+        TextView telefono = (TextView) findViewById(R.id.telefonoContacto);
+        ImageView foto = (ImageView) findViewById(R.id.fotoContacto);
+
+        nombre.setText(getNombre(uri));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                contactoUri = data.getData();
+
+                recibirConctacto(contactoUri);
+            }
+        }
+    }
+
+    private String getNombre(Uri uri) {
+        String nombre = null;
+
+        ContentResolver contentResolver = getContentResolver();
+
+        Cursor c = contentResolver.query(
+                uri,
+                new String[]{Contacts.DISPLAY_NAME},
+                null,
+                null,
+                null
+        );
+
+        if (c.moveToFirst()) {
+            nombre = c.getString(0);
+        }
+        c.close();
+
+        return nombre;
+    }
+
 }
+
+
+
+
