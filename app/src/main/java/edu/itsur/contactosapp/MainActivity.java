715c78d7bc9ce53615c10bd4c24f04cd7d97a951
ozwaldo <ozwaldo@gmail.com>
2019-02-01 12:54:27 -0600

    01/02/2019

--- app/src/main/java/edu/itsur/contactosapp/MainActivity.java
+++ app/src/main/java/edu/itsur/contactosapp/MainActivity.java
@@ -1,11 +1,18 @@
 package edu.itsur.contactosapp;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.Contacts;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,17 +20,45 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import static android.provider.ContactsContract.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int PICK_CONTACT_REQUEST = 1;
+    private final int PERMISSIONS_REQUEST_READ_CONTACT = 100;
     private Uri contactoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new
+                            String[]{Manifest.permission.READ_CONTACTS},
+                    PERMISSIONS_REQUEST_READ_CONTACT
+            );
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_CONTACT:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    return;
+                } else {
+                    finish();
+                }
+                return;
+        }
     }
 
     public void initSeleccionarContacto(View v) {
@@ -38,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
 
         nombre.setText(getNombre(uri));
         telefono.setText(getTelefono(uri));
+        foto.setImageBitmap(getFoto(uri));
     }
 
     @Override
@@ -106,9 +142,38 @@ public class MainActivity extends AppCompatActivity {
             telefono = telefonoCursor.getString(0);
         }
 
+        telefonoCursor.close();
+
         return telefono;
     }
 
+    private Bitmap getFoto(Uri uri){
+        Bitmap foto = null; String id = null;
+        Cursor contactoCursor = getContentResolver().query(
+                uri,
+                new String[]{Contacts._ID},
+                null,null,null
+        );
+        if (contactoCursor.moveToFirst()){
+            id = contactoCursor.getString(0);
+        }
+        contactoCursor.close();
+        try{
+            Uri contacctoUri = ContentUris.withAppendedId(
+                    Contacts.CONTENT_URI,
+                    Long.parseLong(id)
+            );
+            InputStream input = Contacts.openContactPhotoInputStream(
+                    getContentResolver(),
+                    contacctoUri
+            );
+            if (input != null) {
+                foto = BitmapFactory.decodeStream(input);
+                input.close();
+            }
+        }catch (IOException ioe){ }
+        return foto;
+    }
 
 }
 
